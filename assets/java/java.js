c173756acc5f208d489ad2e7546d7ce43c52207a
melissa rice <rice.jean29@gmail.com>
2018-04-15 20:43:15 -0600

    version2

---
+++ assets/java/java.js
@@ -0,0 +1,66 @@
+
+//hangman
+$( "#hangRun" ).click(function() {
+    window.open("https://gazer89.github.io/Hangman/");
+  });
+$( "#hangCode" ).click(function() {
+    window.open("https://github.com/Gazer89/Hangman");
+  });
+
+//starwars rpg
+$( "#starRun" ).click(function() {
+    window.open("https://gazer89.github.io/week-4-game/");
+  });
+$( "#starCode" ).click(function() {
+    window.open("https://github.com/Gazer89/week-4-game");
+  });
+
+//harry potter triva
+$( "#trivaRun" ).click(function() {
+    window.open("https://gazer89.github.io/triva/");
+  });
+$( "#trivaCode" ).click(function() {
+    window.open("https://github.com/Gazer89/triva");
+  });
+
+//train sch widget
+$( "#trainRun" ).click(function() {
+    window.open("https://gazer89.github.io/train_arrivals/");
+  });
+$( "#trainCode" ).click(function() {
+    window.open("https://github.com/Gazer89/train_arrivals");
+  });
+
+//dater app
+$( "#apiRun" ).click(function() {
+    window.open("https://gazer89.github.io/Dater_App/");
+  });
+$( "#apiCode" ).click(function() {
+    window.open("https://github.com/Gazer89/Dater_App");
+  });
+
+//liri terminal app
+$( "#siriRun" ).click(function() {
+    alert("OOPs, you need to run this in your terminal, please click the Code button.")
+  });
+$( "#siriCode" ).click(function() {
+    window.open("https://github.com/Gazer89/liri-node-app/blob/master/README.md");
+  });
+
+  //giphy search app
+$( "#giphyRun" ).click(function() {
+    window.open("https://gazer89.github.io/GIPHY_API/");
+  });
+$( "#giphyCode" ).click(function() {
+    window.open("https://github.com/Gazer89/GIPHY_API");
+  });
+
+//tbd app
+$( "#tbdRun" ).click(function() {
+    alert("OOPs, this app is not ready yet, please come back later.")
+  });
+$( "#tbdCode" ).click(function() {
+    alert("OOPs, this app is not ready yet, please come back later.")
+  });
+
+  $('[data-toggle="tooltip"]').tooltip(); 
